f9b9b7d5d48c0e1461b2c5ede4316bf53489da46
heehun.kang <skypacific@gmail.com>
2019-01-28 18:52:58 +0900

    modify style

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,15 +9,7 @@
     $(document).ready(function(){
 
         $(".post-content").fitVids();
-
-        // Calculates Reading Time
-        var txt = $('<div></div>').hide().text($('.post-content p, .post-content li').text()).appendTo($('body'));
-        txt.readingTime({
-            readingTimeTarget: '.post-reading-time',
-            wordCountTarget: '.post-word-count',
-            wordsPerMinute: 200,
-        });
-
+        
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
